51f701241f994dc7a84c78c6b9c4e54212c243f5
AntoineGiraud <antoine.giraud@outlook.com>
2025-09-05 18:29:05 +0200

    feat(offload): bulk2 offload of salesforce object/table

---
+++ sf_offload_objects.py
@@ -0,0 +1,41 @@
+from SalesforceHelper import SalesforceHelper
+import polars as pl
+from pathlib import Path
+import time
+import sys
+# import duckdb
+
+# Ajouter le dossier parent au sys.path
+sys.path.insert(0, "..")
+from timer import Timer
+
+# Connexion à Salesforce
+sf = SalesforceHelper.from_yml()
+
+
+# -----------------------------------------------------------------------------
+# bulk_2 exports -< outputs .csv chuncks of sf table
+# -----------------------------------------------------------------------------
+
+# sf.fetch_bulk_v2("Opportunity2__c")
+# sf.fetch_bulk_v2("Devis_type__c")
+# sf.fetch_bulk_v2("Lignes_de_devis_type__c")
+# sf.fetch_bulk_v2("Product2")
+sf.fetch_bulk_v2("PricebookEntry")
+
+# -----------------------------------------------------------------------------
+# query_all exports -> slower
+# -----------------------------------------------------------------------------
+
+# def query_and_offload(table: str) -> pl.DataFrame:
+#     df = sf.query_all(table, limit=100000).select(pl.exclude("attributes")).cast(pl.String)
+#     print(f"{df=}")
+#     df.write_csv("offload/df_{table}_{sf.env}.csv")
+#     df.write_parquet(f"offload/df_{table}_{sf.env}.parquet")
+#     # duckdb.sql("copy (from df) to 'offload/df_Product2.csv'")
+
+
+# query_and_offload('Product2')
+# query_and_offload('Devis_type__c')
+# query_and_offload('Lignes_de_devis_type__c')
+# query_and_offload("Opportunity2__c")
